03088b3835e25d06b31e947ce2033dc7253d22c1
Sebastian Gutierrez <orjuela.desarrollador@gmail.com>
2021-05-23 09:31:49 -0500

    Mostrando el form en el template

---
+++ manager_messages/form.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import fields
+
+from .models import Message
+
+class MessageForm(forms.ModelForm):
+
+    class Meta:
+        model = Message
+        fields = ('author_name', "message")
