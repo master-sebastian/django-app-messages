95426790f45f304e294b61ec04f83b1e448675b3
Sebastian Gutierrez <orjuela.desarrollador@gmail.com>
2021-05-23 09:32:45 -0500

    Mostrando el form en el template parte 2

--- basic_project/form.py
+++ basic_project/form.py
@@ -1,8 +1,9 @@
 from django import forms
 
-class ProfileForm(forms.Form):   
-    first_name = forms.CharField(max_length=100, required=True, label="Primer nombre")
-    last_name = forms.CharField(max_length=100, required=True, label="Segundo nombre")
-    bibliography = forms.CharField(max_length=500, required=True, label="bibliografia")
-    birthdate = forms.DateField(required=False, label="Fecha de nacimiento")
-    picture = forms.ImageField(required=False, label="Image de perfil")
+class ProfileForm(forms.Form):
+    #Usand el parametro label puede cambiar el nombre a otro idioma manualmente
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    bibliography = forms.CharField(max_length=500, required=True)
+    birthdate = forms.DateField(required=False)
+    picture = forms.ImageField(required=False)
