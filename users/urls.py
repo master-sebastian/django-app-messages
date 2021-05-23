d9c2a43c519b5197d6bf04f1ed771a4b087cf054
Sebastian Gutierrez <orjuela.desarrollador@gmail.com>
2021-05-23 16:53:16 -0500

    cargando solo template

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
+import django
 from django.urls import path
 from . import views as views
-
-
+from django.views.generic import TemplateView
 #Views
 urlpatterns = [
-
+    path(
+        route="profile/<str:username>/",
+        view=TemplateView.as_view(template_name='users/detail.html'),
+        name="detail"
+    ),
     path(
         "login/", 
         views.loginApp, 
